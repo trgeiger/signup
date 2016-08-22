e3ca2dc7b83e6e6c5f9a91d92872718b2c31cc11
Tayler Geiger <taylergeiger@gmail.com>
2016-08-22 14:51:22 -0500

    got entire signup page working

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import webapp2
 import cgi
+import re
 
 # html boilerplate for the top of every page
 page_header = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Caesar Cipher</title>
+    <title>Signup</title>
     <style type="text/css">
         .error {
             color: red;
@@ -15,7 +16,7 @@ page_header = """
 </head>
 <body>
     <h1>
-        <a href="/">Caesar Cipher</a>
+        <a href="/">Signup</a>
     </h1>
 """
 
@@ -25,84 +26,117 @@ page_footer = """
 </html>
 """
 
+signup_form = """
+<form action="/" method="post">
+    <table>
+        <tbody>
+            <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username" autofocus required value= %(user)s >
+                <span class="error">%(uerror)s</span></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password" pattern="^.{3,20}$" required>
+                <span class="error">%(perror)s</span></td>
+            </tr>
+            <tr>
+                <td>Confirm Password:</td>
+                <td><input type="password" name="verify" pattern="^.{3,20}$" required>
+                <span class="error">%(verror)s</span></td>
+            </tr>
+            <tr>
+                <td>Email (optional):</td>
+                <td><input type="email" name="email" value= %(email)s>
+                <span class="error">%(eerror)s</span></td>
+            </tr>
+        </tbody>
+    </table>
+    <input type="submit" value="Submit">
+</form>
+"""
+
+page = page_header + signup_form + page_footer
 
 class Index(webapp2.RequestHandler):
     """ Handles requests coming in to '/' (the root of our site)
     """
 
+    def write_form(self, uerror="", perror="", verror="", eerror="", user="", email=""):
+        self.response.out.write(page %{"uerror": uerror, "perror": perror, "verror": verror, "eerror": eerror, "user": user, "email": email})
+
     def get(self):
 
-        header = "<h3>For secrets...</h3>"
+        header = "<h3></h3>"
 
-        # a form for implementing rotation cipher
-        cipher_form = """
-        <form action="/rotate" method="post">
-            <fieldset>
-                <legend><strong>Input</strong></legend>
-                Rotation Amount:<br>
-                <input type="text" name="rotation"><br>
-                Phrase:<br>
-                <input type="text" name="phrase"><br><br>
-                <input type="submit" value="Submit">
-            </fieldset>
-        </form>
-        """
-
-        # if we have an error, make a <p> to display it
-        error = self.request.get("error")
-        error_element = "<p class='error'>" + error + "</p>" if error else ""
-
-        # combine all the pieces to build the content of our response
-        main_content = header + cipher_form + error_element
-        response = page_header + main_content + page_footer
-        self.response.write(response)
-
-
-class Signup(webapp2.RequestHandler):
-    """ Handles requests coming in to '/signup'
-    """
+        self.write_form()
 
     def post(self):
         # look inside the request to figure out what the user typed
-        phrase = self.request.get("phrase")
-        rotation = self.request.get("rotation")
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
 
-        # if the user typed nothing at all, redirect and yell at them
-        if (phrase == "") and (rotation == ""):
-            error = "You didn't type anything."
-            self.redirect("/?error=" + error)
+        # regular expressions for user input verification
+        username_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        password_re = re.compile(r"^.{3,20}$")
+        email_re = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 
-        if (phrase == "") or (rotation== ""):
-            error = "You didn't type anything."
-            self.redirect("/?error=" + error)
+        def valid_input(input, regexp):
+            return regexp.match(input)
 
-        #check if rotation is a numeral
-        if rotation.isdigit():
-            rotation = int(rotation)
-        elif rotation.isalpha():
-            error = "Your rotation must be an integer."
-            self.redirect("/?error=" + error)
+        # if the user typed invalid username, give error
+        if not valid_input(username, username_re):
+            uerror = "Invalid username"
+        else:
+            uerror = ""
 
-        # if the user wants to cipher a phrase that contains numerals, display error
-        if not all(x.isalpha() or x.isspace() for x in phrase):
-            error = "Your phrase can only contain alphas and spaces."
-            self.redirect("/?error=" + error)
+        # if the user typed invalid password, give error
+        if not valid_input(password, password_re):
+            perror = "Invalid password"
+        else:
+            perror = ""
+
+        # check if password matches verify
+        if password != verify:
+            verror = "Please reconfirm your password"
+        else:
+            verror = ""
+
+        #if user typed invalid email address, give error
+        if (email != "") and (not valid_input(email, email_re)):
+            eerror = "Invalid email address"
+        else:
+            eerror = ""
+
+        # if any of the above errors are triggered, preserve user input and clear password, display error messages
+        if not (valid_input(username, username_re) and valid_input(password, password_re) and (password == verify)):
+            self.write_form(uerror, perror, verror, eerror, username, email)
+        else:
+            self.redirect("/welcome?username=" + username)
+
+
+
+
+class Welcome(webapp2.RequestHandler):
+    """ Handles requests coming in to '/welcome'
+    """
+
+    def get(self):
+        # look inside the request to figure out what the user typed
+        username = self.request.get("username")
 
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-        phrase = cgi.escape(phrase, quote=True)
-        #rotation = cgi.escape(rotation, quote=True)
-
-        # use the caesar.py file to enact the cipher using phrase and rotation
-        if all(x.isalpha() or x.isspace() for x in phrase) and (type(rotation) is int):
-            new_phrase = caesar.encrypt(phrase, rotation)
+        username = cgi.escape(username, quote=True)
 
         # build response content
-            new_phrase_element = "<h2>Your encrypted phrase:</h2><br>" + "<p>" + new_phrase + "</p>"
-            response = page_header + "<p>" + new_phrase_element + "</p>" + page_footer
-            self.response.write(response)
+        confirm_signup_message = "Welcome " + username + "!"
+        response = page_header + "<p>" + confirm_signup_message + "</p>" + page_footer
+        self.response.write(response)
 
 
 app = webapp2.WSGIApplication([
     ('/', Index),
-    ('/rotate', RotatePhrase)
+    ('/welcome', Welcome)
 ], debug=True)
